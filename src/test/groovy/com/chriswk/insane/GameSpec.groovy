1d744a331f24504f4af1a7028dd64e98c74e4436
Christopher Kolstad <christopher.kolstad@gmail.com>
2012-02-23 18:21:45 +0100

    Green test for empty game

--- src/test/groovy/com/chriswk/insane/GameSpec.groovy
+++ src/test/groovy/com/chriswk/insane/GameSpec.groovy
@@ -5,7 +5,7 @@ import spock.lang.*
 class GameSpec extends Specification {
     def "it should score an empty game as zero"() {
         expect:        
-        game.score() == 15
+        game.score() == 0
 
         where:
         game = new Bowling()
