a8b439a31deceebd2c04b30852fd6d887a0e9053
Christopher Kolstad <christopher.kolstad@gmail.com>
2012-02-23 18:29:59 +0100

    use FunSuite

---
+++ src/test/scala/com/chriswk/insane/BowlingSuite.scala
@@ -0,0 +1,12 @@
+package com.chriswk.insane
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class BowlingSpec extends FlatSpec with ShouldMatchers {
+    
+    "A bowling game" should "score an empty game as 0" in {
+        val game = new Bowling
+        game.score should equal (15)
+    }
+}
