3d2862e015294d2d24a27192cbe4ed65982bf532
Christopher Kolstad <christopher.kolstad@gmail.com>
2012-02-23 16:14:51 +0100

    Initial project layout

---
+++ src/main/groovy/com/chriswk/insane/Bowling.groovy
@@ -0,0 +1,7 @@
+
+class Bowling {
+    
+    def score() {
+        0
+    }
+}
---
+++ src/test/java/com/chriswk/insane/BowlingTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class BowlingTest {
+    Bowling b;
+
+    @Before
+    public void setUp() {
+        b = new Bowling();
+    }
+
+    @Test
+    public void shouldScoreEmptyGameAsZero() {
+        assertEquals(0, b.score());
+    }
+}
