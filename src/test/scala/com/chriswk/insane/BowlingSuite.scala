318c1b0ca80cfc1af6de936d98a93014cfe75336
Christopher Kolstad <christopher.kolstad@gmail.com>
2012-02-23 20:30:48 +0100

    Don't run scalatest on init
    This caused the tests to be run as desired, but caused clean target and
    all other targets dependent on this to fail if ScalaTest run failed.
    
    This was not the desired behaviour.
    
    Still need to find a way to add scalatest as a task before the test task
    generates the reports

--- src/test/scala/com/chriswk/insane/BowlingSuite.scala
+++ src/test/scala/com/chriswk/insane/BowlingSuite.scala
@@ -1,13 +1,39 @@
 package com.chriswk.insane
 
-import org.scalatest.FlatSpec
+import org.scalatest.FunSpec
+import org.scalatest.BeforeAndAfter
 import org.scalatest.matchers.ShouldMatchers
 
-class BowlingSpec extends FlatSpec with ShouldMatchers {
+class BowlingSpec extends FunSpec with ShouldMatchers with BeforeAndAfter {
     
-    "A bowling game" should "score an empty game as 0" in {
-        val game = new Bowling
-        game.score should equal (0)
+    var game : Bowling = _
+
+    before {
+        game = new Bowling
     }
+
+    describe("A bowling game") {
+        it("should score an empty game as 0") {
+            game.score should equal (0)
+        }
+
+        it("should score all 1s as 20") {
+            for(i <- 1 to 20) {
+                game.roll(1)
+            }
+            game.score should equal (20)
+        }
+
+        it("should score 9 and spare as 190") {
+            for(i <- 1 to 10) {
+                game.roll(9)
+                game.roll(1)
+            }
+            game.roll(9)
+            game.score should equal(199)
+        }
+    }
+
+
     
 }
