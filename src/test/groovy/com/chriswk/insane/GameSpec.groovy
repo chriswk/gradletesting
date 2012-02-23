72e0d0d9023818db39a99911b724080249b4d374
Christopher Kolstad <christopher.kolstad@gmail.com>
2012-02-23 18:21:12 +0100

    Red test in GameSpec in groovy classpath

--- src/test/groovy/com/chriswk/insane/GameSpec.groovy
+++ src/test/groovy/com/chriswk/insane/GameSpec.groovy
@@ -3,21 +3,12 @@ package com.chriswk.insane
 import spock.lang.*
 
 class GameSpec extends Specification {
-    def game
-    
-    def setup() {
-        game = new Bowling()
-    }
-
     def "it should score an empty game as zero"() {
-        game.score() == 15 
-    }
+        expect:        
+        game.score() == 15
 
-    def "it should score a game of all ones as 20"() {
-        for(i in 1..20) {
-            game.roll(1)
-        }
-        game.score() == 20
+        where:
+        game = new Bowling()
+
     }
-        
 }
